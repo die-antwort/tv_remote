245b23dd7d273cbc7b9a073e83360badce558ff4
Stefan Daschek <stefan@die-antwort.eu>
2017-12-15 20:07:15 +0100

    Whitespace cleanup

--- server.rb
+++ server.rb
@@ -20,22 +20,22 @@ get '/' do
 end
 
 get '/off' do
-	@tv_remote.power_off
+  @tv_remote.power_off
   redirect to("/")
 end
 
 get '/on' do
-	@tv_remote.power_on
+  @tv_remote.power_on
   sleep 7 # Powering on takes some time …
   redirect to("/")
 end
 
 get '/statusdroid' do
-	@tv_remote.select_channel :statusdroid
+  @tv_remote.select_channel :statusdroid
   redirect to("/")
 end
 
 get '/appletv' do
-	@tv_remote.select_channel :apple_tv
+  @tv_remote.select_channel :apple_tv
   redirect to("/")
 end
--- tv_remote.rb
+++ tv_remote.rb
@@ -16,28 +16,28 @@ class TvRemote
   POLL = "ff"
 
   def power_on
-  	send_command CMD_POWER, "01"
+    send_command CMD_POWER, "01"
   end
 
   def power_off
-  	send_command CMD_POWER, "00"
+    send_command CMD_POWER, "00"
   end
 
   def select_channel(channel)
     channel_nr = CHANNELS.fetch(channel){ raise "Unknown channel '#{channel}'!"}
-  	send_command CMD_INPUT, "a#{channel_nr}"
+    send_command CMD_INPUT, "a#{channel_nr}"
   end
-  
+
   def read_current_channel
-  	result = send_command CMD_INPUT, POLL
+    result = send_command CMD_INPUT, POLL
     result ? CHANNELS.invert[result[8].to_i] : nil
   end
-  
+
   private
-  
+
   def send_command(command, data)
     with_serial_port do |sp|
-    	sp.write "#{command} 01 #{data}\r"
+      sp.write "#{command} 01 #{data}\r"
       if data == POLL
         Timeout::timeout(0.5) do
           sleep 0.1 until (result = sp.gets("x"))
@@ -51,8 +51,8 @@ class TvRemote
   end
 
   def with_serial_port
-  	sp = SerialPort.new(PORT, 9600)
-	  sp.read_timeout = 1000
+    sp = SerialPort.new(PORT, 9600)
+    sp.read_timeout = 1000
     yield sp
   ensure
     sp.close
